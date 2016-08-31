e8824604b2c423e80d3a0e74d0ec988d4b85d4c2
Andrew Duthie <andrew@andrewduthie.com>
2016-08-31 14:29:03 -0400

    Add getNameByUser tests

---
+++ get-name-by-user.test.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import { getNameByUser } from './get-name-by-user';
+
+describe( 'getNameByUser()', () => {
+	it( 'should throw on missing username', () => {
+		expect( () => getNameByUser( 'ockham' ) ).to.throw( TypeError );
+	} );
+
+	it( 'should return a user\'s name', () => {
+		expect( getNameByUser( 'aduth' ) ).to.equal( 'Andrew Duthie' );
+	} );
+} );
