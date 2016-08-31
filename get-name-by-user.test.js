f7271797d1392ef569d800af4a420abea6ef302e
Andrew Duthie <andrew@andrewduthie.com>
2016-08-31 14:29:52 -0400

    Test for presence of prototype member

--- get-name-by-user.test.js
+++ get-name-by-user.test.js
@@ -6,6 +6,10 @@ describe( 'getNameByUser()', () => {
 		expect( () => getNameByUser( 'ockham' ) ).to.throw( TypeError );
 	} );
 
+	it( 'should throw on missing username by object prototype member', () => {
+		expect( () => getNameByUser( 'valueOf' ) ).to.throw( TypeError );
+	} );
+
 	it( 'should return a user\'s name', () => {
 		expect( getNameByUser( 'aduth' ) ).to.equal( 'Andrew Duthie' );
 	} );
