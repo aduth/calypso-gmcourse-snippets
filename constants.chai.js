847045f203cf5280e9f864ed761dbbee1befe92e
Andrew Duthie <andrew@andrewduthie.com>
2016-09-02 10:55:13 -0400

    Expand Chai assertions to demonstrate all variants

--- constants.chai.js
+++ constants.chai.js
@@ -1,4 +1,9 @@
-import { assert } from 'chai';
+import { assert, expect, should } from 'chai';
 import { DEFAULT_NAMES as names } from './constants';
 
+should();
+
+assert.equal( names[ 'ockham' ], 'Bernie Reiter' );
 assert( names[ 'ockham' ] === 'Bernie Reiter' );
+expect( names[ 'ockham' ] ).to.equal( 'Bernie Reiter' );
+names.should.have.ownProperty( 'ockham' );
