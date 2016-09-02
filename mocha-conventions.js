12095345684aa1055399db5a8320e419fbda349b
Andrew Duthie <andrew@andrewduthie.com>
2016-09-02 10:03:39 -0400

    Add Mocha conventions example

---
+++ mocha-conventions.js
@@ -0,0 +1,10 @@
+describe( 'Array', () => {
+	describe( '.prototype', () => {} );
+	describe( '.from()', () => {} );
+	describe( '#length', () => {} );
+	describe( '#concat()', () => {} )
+} );
+
+describe( 'selectors', () => {
+	describe( 'isSitePreviewable()', () => {} );
+} );
