7a7800070aafd240ba39e71a5a7c6440dff19e0a
Andrew Duthie <andrew@andrewduthie.com>
2016-09-07 08:25:04 -0400

    Add emitter pseudo-code example

---
+++ emitter-pseudo-code.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import { EventEmitter } from 'events/';
+
+class SitesList extends EventEmitter {
+	get() {
+		if ( ! this.data ) {
+			this.fetch();
+		}
+
+		return this.data || [];
+	}
+
+	fetch() {
+		this.data = await wpcom.me.sites();
+		this.emit( 'change' );
+	}
+}
+
+class MyComponent extends Component {
+	componentDidMount() {
+		this.props.sites.on( 'change', this.forceUpdate );
+	}
+
+	render() {
+		return this.props.sites.get().map( ( site ) => (
+			<div key={ site.ID }>{ site.name }</div>
+		) );
+	}
+}
+
+ReactDom.render( MyComponent, { sites: new SitesList() }, document.body );
