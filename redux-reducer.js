09d7aacf454839fbb9e3ab5993e99368d576ccc0
Andrew Duthie <andrew@andrewduthie.com>
2016-09-08 10:45:41 -0400

    Add comments demonstrating reducer output

--- redux-reducer.js
+++ redux-reducer.js
@@ -6,3 +6,7 @@ export default function sites( state = null, action ) {
 
 	return state;
 }
+
+// sites( undefined, { type: 'USER_RECEIVE' } )                 => null
+// sites( null,      { type: 'SITES_RECEIVE', sites: [] } )     => []
+// sites( [],        { type: 'SELECTED_SITE_SET', siteId: 1 } ) => []
