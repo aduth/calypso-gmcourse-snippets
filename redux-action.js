cf58380b8bdd3e9c29111da8ac4dc176a9728dcc
Andrew Duthie <andrew@andrewduthie.com>
2016-09-08 16:22:26 -0400

    Extract action creator from sites receive action

--- redux-action.js
+++ redux-action.js
@@ -3,10 +3,14 @@ import reducer from './reducer';
 
 const store = createStore( reducer );
 
-store.dispatch( {
-	type: 'SITES_RECEIVE',
-	sites: [
-		{ ID: 2916284, name: 'WordPress.com Example Blog' },
-		{ ID: 72145503, name: 'Andrew Duthie Test Site' }
-	]
-} );
+function receiveSites( sites ) {
+	return {
+		type: 'SITES_RECEIVE',
+		sites
+	};
+}
+
+store.dispatch( receiveSites( [
+	{ ID: 2916284, name: 'WordPress.com Example Blog' },
+	{ ID: 72145503, name: 'Andrew Duthie Test Site' }
+] ) );
