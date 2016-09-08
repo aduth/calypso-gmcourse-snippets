07c7e0bf07ba7daf8ca74e164d0a8d5f4b2ba97a
Andrew Duthie <andrew@andrewduthie.com>
2016-09-08 16:15:33 -0400

    Demonstrate action dispatch

--- redux-action.js
+++ redux-action.js
@@ -1,7 +1,12 @@
-const action = {
+import { createStore } from 'redux';
+import reducer from './reducer';
+
+const store = createStore( reducer );
+
+store.dispatch( {
 	type: 'SITES_RECEIVE',
 	sites: [
 		{ ID: 2916284, name: 'WordPress.com Example Blog' },
 		{ ID: 72145503, name: 'Andrew Duthie Test Site' }
 	]
-};
+} );
