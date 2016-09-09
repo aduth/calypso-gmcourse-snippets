4400ff0be118d84ebad7e2ab6eea45b2a3beb033
Andrew Duthie <andrew@andrewduthie.com>
2016-09-09 10:36:37 -0400

    Disable serialization in reducer example

--- redux-reducer.js
+++ redux-reducer.js
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux';
 
 export function requesting( state = false, action ) {
 	switch ( action.type ) {
-		case 'SITES_REQUEST':
+		case SITES_REQUEST:
 			return true;
 
-		case 'SITES_REQUEST_SUCCESS':
-		case 'SITES_REQUEST_FAILURE':
+		case SITES_REQUEST_SUCCESS:
+		case SITES_REQUEST_FAILURE:
+			return false;
+
+		case DESERIALIZE:
+		case SERIALIZE:
 			return false;
 	}
 
@@ -15,7 +19,7 @@ export function requesting( state = false, action ) {
 
 export function items( state = null, action ) {
 	switch ( action.type ) {
-		case 'SITES_RECEIVE':
+		case SITES_RECEIVE:
 			return action.sites;
 	}
 
