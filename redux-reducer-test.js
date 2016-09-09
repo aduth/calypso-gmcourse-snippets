2d662bdeca1c683223b85c4f58fb3ae7e3e2b329
Andrew Duthie <andrew@andrewduthie.com>
2016-09-09 12:27:43 -0400

    Add empty reducer tests

---
+++ redux-reducer-test.js
@@ -0,0 +1,38 @@
+describe( 'reducer', () => {
+	describe( 'requesting', () => {
+		it( 'should default to false', () => {
+		} );
+
+		it( 'should return true on request start', () => {
+		} );
+
+		it( 'should return false on request succeed', () => {
+		} );
+
+		it( 'should return false on request failed', () => {
+		} );
+
+		it( 'should return default state on serialize', () => {
+		} );
+
+		it( 'should return default state on deserialize', () => {
+		} );
+	} );
+
+	describe( 'items', () => {
+		it( 'should default to null', () => {
+		} );
+
+		it( 'should return object of received sites by ID', () => {
+		} );
+
+		it( 'should return same state on serialize', () => {
+		} );
+
+		it( 'should return same state on valid deserialize state', () => {
+		} );
+
+		it( 'should return default state on invalid deserialize state', () => {
+		} );
+	} );
+} );
