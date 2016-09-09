bf2aee03a2151a01c846788956f4b64ac859a521
Andrew Duthie <andrew@andrewduthie.com>
2016-09-09 09:17:13 -0400

    Replace Redux middleware example with thunk

--- redux-middleware.js
+++ redux-middleware.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducer';
 
-const logger = ( store ) => ( next ) => ( action ) => {
-	console.log( 'dispatching', action );
-	const result = next( action );
-	console.log( 'next state', store.getState() );
-	return result;
+const thunk = ( store ) => ( next ) => ( action ) => {
+	if ( 'function' === typeof action ) {
+		return action( store.dispatch, store.getState );
+	}
+
+	return next( action );
 };
 
-createStore( reducer, applyMiddleware( logger ) );
+createStore( reducer, applyMiddleware( thunk ) );
